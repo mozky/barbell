a198927db514b8f9c77101ab303fb12d58c6a744
Javier Suarez <javiersuarezl@outlook.com>
2015-12-08 17:35:00 -0600

    Multiple steps modal for registration
    Added wizard like using a multi step modal. basic functionality and with
    a dark theme.

--- app/client/app/appLayout.js
+++
@@ -1,7 +0,0 @@
-if (Meteor.isClient) {
-  Template.appLayout.events({
-    'click .myProfile': function(event) {
-        Router.go('/user/'+ Meteor.user().username);
-    }
-  });
-}
---
+++ app/client/appLayout/modals/register.js
@@ -0,0 +1,111 @@
+if (Meteor.isClient) {
+  Template.register.events({
+    //TODO:
+    //Confirmation - error message on submit
+    //Validate password match and correct email and unique username
+    //Redirect to profile form
+
+      "submit #registrationForm": function(event){
+          event.preventDefault();
+          var usernameVar = event.target.registerUsername.value;
+          var emailVar = event.target.registerEmail.value;
+          var passwordVar = event.target.registerPassword.value;
+          var passwordConfirmationVar = event.target.registerPasswordConfirmation.value;
+          var nameVar = event.target.registerName.value;
+          var ageVar = event.target.registerAge.value;
+          if (passwordVar !== passwordConfirmationVar) {
+            console.log("Passwords dont match. TODO prevent register.");
+          }
+          console.log("Form submitted.");
+          Accounts.createUser({
+            email: emailVar,
+            password: passwordVar,
+            username: usernameVar,
+            profile: {
+              name: nameVar,
+              age: ageVar
+            }
+          });
+      }
+  });
+
+  Template.register.onRendered(function () {
+    console.log("Rendered");
+    /*
+        Form validation
+    */
+    $('.registrationForm input[type="text"], .registrationForm textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+
+    $('.registrationForm').on('submit', function(e) {
+
+    	$(this).find('input[type="text"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+
+    });
+
+
+		/*
+				Form
+		*/
+		$('.registrationForm fieldset:first-child').fadeIn('slow');
+
+		$('.registrationForm input[type="text"], .registrationForm input[type="password"], .registrationForm textarea').on('focus', function() {
+			$(this).removeClass('input-error');
+		});
+
+		// next step
+		$('.registrationForm .btn-next').on('click', function() {
+			var parent_fieldset = $(this).parents('fieldset');
+			var next_step = true;
+
+			parent_fieldset.find('input[type="text"], input[type="password"], textarea').each(function() {
+				if( $(this).val() == "" ) {
+					$(this).addClass('input-error');
+					next_step = false;
+				}
+				else {
+					$(this).removeClass('input-error');
+				}
+			});
+
+			if( next_step ) {
+				parent_fieldset.fadeOut(400, function() {
+					$(this).next().fadeIn();
+				});
+			}
+
+		});
+
+		// previous step
+		$('.registrationForm .btn-previous').on('click', function() {
+			$(this).parents('fieldset').fadeOut(400, function() {
+				$(this).prev().fadeIn();
+			});
+		});
+
+		// submit
+		$('.registrationForm').on('submit', function(e) {
+
+			$(this).find('input[type="text"], input[type="password"], textarea').each(function() {
+				if( $(this).val() == "" ) {
+					e.preventDefault();
+					$(this).addClass('input-error');
+				}
+				else {
+					$(this).removeClass('input-error');
+				}
+			});
+
+		});
+  });
+
+}
