19d7677dd558ceecaabca31c0cc9c996466dd383
Javier Suarez <javiersuarezl@outlook.com>
2016-04-26 19:01:15 -0500

    Purged gh-pages branch
    Removed app and all external landing page code from the gh-pages branch,
    so we can use a new master branch for the app code.

--- app/server/accounts.js
+++
@@ -1,9 +0,0 @@
-ServiceConfiguration.configurations.remove({
-  service: "instagram"
-});
-ServiceConfiguration.configurations.insert({
-  service: "instagram",
-  clientId: "43aab78e943c4c71937e71078e762328",
-  scope: 'basic',
-  secret: "9be46da44cfe4f5fb0c28ddf38792631"
-});
