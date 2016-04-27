570a238ac3243de20a764f58e55566a63b2c101e
Javier Suarez <javiersuarezl@outlook.com>
2016-04-26 20:10:55 -0500

    Barbell info edit
    Edited the basic app information to match barbell.

--- README.md
+++ README.md
@@ -1,17 +1,6 @@
-# The Meteor Chef - Base
-A starting point for Meteor apps.
+# Barbell
+:muscle: App that stores personal weightlifting records to keep track of progress.
 
-<table>
-  <tbody>
-    <tr>
-      <th>Base Version</th>
-      <td>v4.0.0</td>
-    </tr>
-    <tr>
-      <th>Meteor Version</th>
-      <td>v1.3.2.4</td>
-    </tr>
-  </tbody>
-</table>
+####Thanks to:
 
-[Read the Documentation](http://themeteorchef.com/base)
+[The Meteor Chef - Base](http://themeteorchef.com/base)
--- imports/ui/pages/index.js
+++ imports/ui/pages/index.js
@@ -3,9 +3,9 @@ import { Jumbotron } from 'react-bootstrap';
 
 export const Index = () => (
   <Jumbotron className="text-center">
-    <h2>Base</h2>
-    <p>A starting point for Meteor applications.</p>
-    <p><a className="btn btn-success" href="https://themeteorchef.com/base" role="button">Read the Documentation</a></p>
-    <p style={ { fontSize: '16px', color: '#aaa' } }>Currently at v4.0.0</p>
+    <h2>Barbell</h2>
+    <p>Lift and track.</p>
+    <p><a className="btn btn-success" href="#" role="button">Add new record</a></p>
+    <p style={ { fontSize: '16px', color: '#aaa' } }>Alpha 0.0.1</p>
   </Jumbotron>
 );
