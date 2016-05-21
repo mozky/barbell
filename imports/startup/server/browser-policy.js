45786c3a8e9e4e42949684b043b58204d26f09f1
Javier Suarez <javiersuarezl@outlook.com>
2016-05-20 19:09:45 -0500

    Added browser policy rules to allow google fonts and youtube.com content

--- imports/startup/server/browser-policy.js
+++ imports/startup/server/browser-policy.js
@@ -1,3 +1,7 @@
-// import { BrowserPolicy } from 'meteor/browser-policy-common';
+import { BrowserPolicy } from 'meteor/browser-policy-common';
 
-// e.g., BrowserPolicy.content.allowOriginForAll( 's3.amazonaws.com' );
+BrowserPolicy.content.allowFrameOrigin('www.youtube.com');
+BrowserPolicy.content.allowOriginForAll('fonts.googleapis.com');
+BrowserPolicy.content.allowImageOrigin('placehold.it');
+BrowserPolicy.content.allowImageOrigin('placeholdit.imgix.net');
+BrowserPolicy.content.allowFontOrigin('fonts.gstatic.com');
